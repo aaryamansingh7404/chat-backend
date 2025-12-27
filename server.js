34a3c501025e2054370b2b27845d6bb4e8726bc2
Aaryaman Singh <aaryamansingh49@gmail.com>
2025-12-27 14:36:05 +0530

    server

--- server.js
+++ server.js
@@ -80,15 +80,15 @@ io.on("connection", (socket) => {
   });
 
   // 🟢 SEEN: Only when real receiver opens chat
-  socket.on("chatOpened", ({ user1, user2 }) => {
-    const room = [user1.trim(), user2.trim()].sort().join("_");
-  
+  socket.on("chatOpened", ({ opener, partner }) => {
+    if (!opener || !partner) return;
+    const room = [opener.trim(), partner.trim()].sort().join("_");
     io.to(room).emit("updateAllSeen", {
-      opener: user1,     // jiska screen currently open
-      partner: user2,    // jisko messages bheje the
+      opener,
+      partner,
+      status: "seen",
     });
   });
-  
 
   // 🟢 ONLINE / OFFLINE STATUS
   socket.on("userOnline", ({ userName }) => {
